30c891d0bdda0e852499d28641301aea213bcc92
mandeep <13114425man@gmail.com>
2022-12-26 22:50:17 +0530

    Latest commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,21 @@
 import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
-import {ConnectButton , Icon, Select, DatePicker, Input, Button} from "web3uikit";
+import {ConnectButton , Icon, Select, DatePicker, Input, Button, CryptoCards, Skeleton} from "web3uikit";
 import { useState } from "react";
 
 
+// bootstrap..........................................................................................................
+
+
+
+
+// bootstrap.........................................................................................................
+
 import bg from "../assets/images/R.jpg";
 import logo from "../assets/images/p.png";
+
+
 
 // extra...  
 
@@ -15,6 +24,19 @@ const Home = () => {
   const [checkOut, setCheckOut]=useState(new Date());
   const [destination, setDestination]=useState(new Date());
   const [guests, setGuests]=useState(1);
+
+  // by me
+
+  const images = [
+    { url: "https://unsplash.com/photos/7KLa-xLbSXA" },
+    { url: "..assests/images/2.JPG" },
+    { url: "../assets/images/R.jpg" },
+    { url: "../assets/images/R.jpg" },
+    { url: "../assets/images/R.jpg" },
+    { url: "../assets/images/R.jpg" },
+    { url: "../assets/images/R.jpg" },
+  ];
+  // ..............................................................
   
 
   return (
@@ -117,7 +139,7 @@ const Home = () => {
                       numberMin: 1
                     }} />
         </div>
-        
+
                     {/* link to another page */}
         <Link to={"/Rentals"} state={{
           destination: destination,
@@ -142,11 +164,57 @@ const Home = () => {
         onClick={()=> console.log(checkOut)}
         />
      </div>
+     {/* CARDS  ACCEPTED */}
 
      <div className="randomContainer">
-      jjj
 
-     </div>
+
+      {/* Crypto cards */}
+
+      
+     <CryptoCards
+  bgColor="linear-gradient(113.54deg, rgba(117, 183, 251, 0.531738) 14.91%, rgba(215, 38, 243, 0.6) 42.57%, rgba(252, 84, 255, 0.336) 45.98%, rgba(209, 103, 255, 0.03) 55.76%), linear-gradient(160.75deg, #AB42CB 41.37%, #45FFFF 98.29%)"
+  btnText="View Endpoints"
+  chain="ethereum"
+  chainType="Network"
+  onClick={function noRefCheck(){}}
+/>
+<CryptoCards
+  bgColor="linear-gradient(113.54deg, rgba(217, 166, 54, 0.6) 14.91%, rgba(230, 166, 26, 0.6) 44.27%, rgba(207, 168, 28, 0) 45.98%, rgba(250, 228, 30, 0) 55.76%, rgba(245, 223, 30, 0) 55.76%), linear-gradient(147.17deg, #F5D116 48.73%, #CD9614 98.22%)"
+  btnText="View Endpoints"
+  chain="binance"
+  chainType="Network"
+  onClick={function noRefCheck(){}}
+/>
+<CryptoCards
+  bgColor="linear-gradient(113.54deg, rgba(103, 58, 194, 0.6) 14.91%, rgba(122, 74, 221, 0.498) 44.27%, rgba(170, 129, 255, 0.222) 45.98%, rgba(209, 103, 255, 0.03) 55.76%), linear-gradient(160.75deg, #7A4ADD 41.37%, #D57BFF 98.29%)"
+  btnText="View Endpoints"
+  chain="polygon"
+  chainType="Network"
+  onClick={function noRefCheck(){}}
+/>
+<CryptoCards
+  bgColor="linear-gradient(113.54deg, rgba(119, 0, 1, 0.5) 14.91%, rgba(216, 43, 44, 0.5) 43.21%, rgba(255, 130, 130, 0.345) 44.27%, rgba(220, 96, 97, 0.185) 55.76%), linear-gradient(151.07deg, #8F0E0F 33.25%, #FA4A4B 98.24%)"
+  btnText="View Endpoints"
+  chain="avalanche"
+  chainType="Network"
+  onClick={function noRefCheck(){}}
+/>
+
+</div>
+
+          <div className="randomContainer">
+            
+                  <div>
+                    ...
+                    
+                  </div>
+                    <div>
+                      <Skeleton theme="image"/>
+                                            
+                    </div>
+
+          </div>
     </>
   );
 
